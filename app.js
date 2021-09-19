750bb8b3d39aa8885d011f914f2f4d19a0b7b91e
Emely Alonzo <68464035+emelyalonzo@users.noreply.github.com>
2021-09-19 20:13:08 +0200

    fixed the bugs and the scent problem
    Consideration: when a robot goes out the grid, it can't never come back, it is now on the dark side ;)

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
+//The robots move by running this function, so we import it 
 const move = require("./envir.js");
+//For a CLI approach, is used the readline module
 const readline = require('readline');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-let result = "";
-const cardinalPoints = 'NESW'; //if we go clockwise, these are the possible orientations
-let lostRobots = [] // This array will look like [[1,2],[3,4]]
-let scent = [];
-let robots = [];
-const upperRight = [];
-
-
-class Robot {
-  constructor(x, y, orientation, instructions, lost) {
-    this.x = x;
-    this.y = y;
-    this.orientation = orientation;
-    this.instructions = instructions;
-    this.lost = lost;
-  }
-}
 
 function input(prompt) {
     return new Promise((callback, error) => {
@@ -31,39 +16,60 @@ function input(prompt) {
             error();
         });
     });
-}
+};
+
+class Robot {
+    constructor(x, y, orientation, instructions, lost) {
+      this.x = x;
+      this.y = y;
+      this.orientation = orientation;
+      this.instructions = instructions;
+      this.lost = lost;
+    }
+};
+  
+let result = "";
+//For taking the orientation information, going clockwise, these are the possible orientations numbered by 0123 for NESW
+const cardinalPoints = 'NESW'; 
+let lostRobots = []; //[[x,y],[x2,y2]...]
+let scent = []; //[[x,y,cardinalNumber],[x2,y2,cardinalNumber]...]
+let robots = []; //[{object1},{object2}]
+const upperRight = []; //[x,y]
 
 function gridBounds (answer){
-    //This function receives the string of the user and save the numbers into an array upperRight
+    //This function receives the string of the user and save the numbers into the array upperRight
     answer = answer.split(" ", 2);
     let int = parseInt(answer[0], 10);
     let int2 = parseInt(answer[1], 10);
     upperRight.push(int);
     upperRight.push(int2);  
-}
+};
 
 function addRobot (answer){
+    //This function adds an instance of Robot to the array robots
     answer = answer.split(" ", 4);
-    //The user says one letter as an orientation, so we convert it to a number
+    //The user says a letter as the orientation (N,E,S,W), so we convert it to a number with cardinalPoints
     let cardinalNum = cardinalPoints.indexOf(answer[2]);
-    //To store the robots information we use instances of Robot Object
-    var newRobot = new Robot(parseInt(answer[0], 10), parseInt(answer[1], 10), cardinalNum, answer[3], false);
+    let newRobot = new Robot(parseInt(answer[0], 10), parseInt(answer[1], 10), cardinalNum, answer[3], false);
     console.log(newRobot);
     robots.push(newRobot);
-}
+};
 
 const main = async () => {
     bounds = await input("Enter the upper-right coordinates: ");
-    gridBounds(bounds); //bounds is a string of the grid's coordinates
-    //Then we ask for each one of the three robot's information
+    gridBounds(bounds); 
+
+    //Then we ask for the three robots
     for (let i = 0; i < 3; i++) {
         robotInfo = await input("Enter the robot coordinates, orientation and instructions: ");
         addRobot(robotInfo); //This function adds an instance of Robot to the array robots 
     }
-    //We move the robots and print the resulting position for each
+
+    //The output
     for (element in robots) {
-        let result= move(robots[element], upperRight[0], upperRight[1], lostRobots, scent)
+        result = move(robots[element], upperRight[0], upperRight[1], lostRobots, scent);
         console.log(result);
+        console.log(scent);
     }
     rl.close();
 };
--- envir.js
+++ envir.js
@@ -1,59 +1,83 @@
-const cardinalPoints = 'NESW'; //if we go clockwise, these are the possible orientations
-// let lostRobots = [] // This array will look like [[1,2],[3,4]]
-
+const cardinalPoints = 'NESW'; 
 
 function turn(instruction, orientation) {
-    //This function changes the orientation of the robot with a string and an int given
-    switch (instruction) {
-        case "R":
-            orientation++;
-            if (orientation === 4){orientation = 0};
-          return orientation;
-        case "L":
-            orientation--;
-            if (orientation === -1){orientation = 3};
-          return orientation;
-        default: // Going straight, so we don't need to change the orientation
-          return orientation;
-      }
-}
+  //This function changes the orientation of the robot with a string and an int given
+  switch (instruction) {
+      case "R":
+          orientation++;
+          if (orientation === 4){orientation = 0};
+        return orientation;
+      case "L":
+          orientation--;
+          if (orientation === -1){orientation = 3};
+        return orientation;
+      default: // Going straight, so we don't need to change the orientation
+        return orientation;
+  };
+};
+
+function goingLost(Robot, boundX, boundY, lostRobots) {
+  if ((Robot.orientation===1 && Robot.x+1 > boundX) || (Robot.orientation===0 && Robot.y+1 > boundY) || (Robot.orientation ===3 && Robot.x-1 < 0) || (Robot.orientation ===2 && Robot.y-1 < 0)){
+      let newLost = [Robot.x,Robot.y]
+      lostRobots.push(newLost)
+      return true //Robot is going to get lost
+  } else {
+      return false //Robot is not going to get lost
+  };
+};
+
+function findArrayInArray(innerArray, outerArray) {
+  const innerArrayString = JSON.stringify(innerArray);
+  let index = 0;
+  const inArray = outerArray.some(function (element) {
+      index ++;
+      return JSON.stringify(element) === innerArrayString;
+  });
+  if (inArray) {
+      return index - 1;
+  } else {
+      return -1;
+  };
+};
 
 module.exports = function move(Robot, boundX, boundY, lostRobots, scent) {
 
+  //Consideration: When a robot gets lost, it is forever because outside the grid there is a huge black hole and no one can came back, the robot has gone to the dark side ;)
   for (let i = 0; i < Robot.instructions.length  && !Robot.lost; i++){
-    const command = Robot.instructions[i]; //This takes the new instruction
-    //position[2] = command;
-    let nO = turn(command, Robot.orientation); //We calculate the new orientation
+    const command = Robot.instructions[i]; 
+    //Calculate the new orientation
+    let nO = turn(command, Robot.orientation); 
     Robot.orientation = nO;
-    //we check if in this position there is a scent
+    //Check if in this position there is a scent
     let position = [Robot.x, Robot.y, Robot.orientation];
-    // if (scent.includes(position) === true) { //This position and orientation has a scent, so we ignore it
-    //      break; // We ignore the instruction because there is prohibition 
-    // }
-    if (findArrayInArray(position, scent) == 1){
-      break;
-    }
-    //We check if this orientation is within the bounds of the grid
+    //Check if there is a scent in the position, if so go to the next instruction
+    if (findArrayInArray(position, scent) !== -1){
+      continue;
+    };
+    //Check if the robot is going to get lost
     if (goingLost(Robot,boundX,boundY,lostRobots)) {
-        //let newScent = [Robot.x,Robot.y,Robot.orientation];
         scent.push(position);
         Robot.lost = true;
-    }
-    switch (Robot.orientation) {
-        case 0:
-          Robot.y++;
-          continue;
-        case 1:
-          Robot.x++;
-          continue;
-        case 2:
-          Robot.y--;
-          continue;
-        case 3:
-          Robot.x--;
-          continue;
-    }
+    };
+    //if moving to another position
+    if (command === "F"){ 
+      switch (Robot.orientation) {
+          case 0:
+            Robot.y++;
+            continue;
+          case 1:
+            Robot.x++;
+            continue;
+          case 2:
+            Robot.y--;
+            continue;
+          case 3:
+            Robot.x--;
+            continue;
+      };
+    };
   };
+
   let result = [Robot.x.toString(), Robot.y.toString()];
   let cardinalPoint = cardinalPoints.substring(Robot.orientation, Robot.orientation+1);
   result.push(cardinalPoint);
@@ -61,29 +85,5 @@ module.exports = function move(Robot, boundX, boundY, lostRobots, scent) {
     result.push("LOST");
   };
   let a = result.join(" ");
-  return a;
-}
-
-function goingLost(Robot, boundX, boundY, lostRobots) {
-    if ((Robot.orientation===1 && Robot.x+1 > boundX) || (Robot.orientation===0 && Robot.y+1 > boundY) || (Robot.orientation ===3 && Robot.x-1 < 0) || (Robot.orientation ===2 && Robot.y-1 < 0)){
-        let newLost = [Robot.x,Robot.y]
-        lostRobots.push(newLost)
-        return true //if the robot is lost
-    } else {
-        return false //if the robot isn't lost
-    }
-}
-
- function findArrayInArray(innerArray, outerArray) {
-   const innerArrayString = JSON.stringify(innerArray);
-   let index = 0;
-   const inArray = outerArray.some(function (element) {
-       index ++;
-       return JSON.stringify(element) === innerArrayString;
-   });
-   if (inArray) {
-       return index - 1;
-   } else {
-       return -1;
-   }
- }
+  return a; //the final output on a string with whitespace
+};
