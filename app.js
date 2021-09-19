98acd9203b4d8a94c39e70987ac8ad9d8b7f3fa7
Emely Alonzo <68464035+emelyalonzo@users.noreply.github.com>
2021-09-19 12:43:49 +0200

    fixed the input and output on the command line
    To do: fix the orientation in the output (reference problem on function move). And fix the final position output.

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
-//We set the object for out robots
-function Robot(x, y, orientation, instructions, lost) {
-  this.x = x;
-  this.y = y;
-  this.orientation = orientation;
-  this.instructions = instructions;
-  this.lost = lost;
-}
-let a = 2; //Since the input is for 3 robots this value is 3-1
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let result = "";
 const cardinalPoints = 'NESW'; //if we go clockwise, these are the possible orientations
 let lostRobots = [] // This array will look like [[1,2],[3,4]]
 let scent = [];
 let robots = [];
 const upperRight = [];
-const turn = require("./envir.js");
-const move = require("./envir.js");
+const move = require("./envir.js").default;
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+function Robot(x, y, orientation, instructions, lost) {
+    this.x = x;
+    this.y = y;
+    this.orientation = orientation;
+    this.instructions = instructions;
+    this.lost = lost;
+  }
 
-// rl.setPrompt('Enter the upper-right coordinates > ');
-// rl.prompt();
-// rl.on('line', (line) => { 
-// line = line.split(" ", 2);
-// upperRight.push(line[0]);
-// upperRight.push(line[1]);  
-// }).on('close', () => {
-// console.log('Have a nice day!');
-// });
+function input(prompt) {
+    return new Promise((callback, error) => {
+        rl.question(prompt, (userInput)=> {
+            callback(userInput);
+        }, ()=> {
+            error();
+        });
+    });
+}
 
-rl.question('Set the upper-right coordinates: ', (answer) => {
-  answer = answer.split(" ", 2);
-  upperRight.push(answer[0]);
-  upperRight.push(answer[1]);  
-});
+function gridBounds (answer){
+    //This function receives the string of the user and save the numbers into an array upperRight
+    answer = answer.split(" ", 2);
+    let int = parseInt(answer[0], 10);
+    let int2 = parseInt(answer[1], 10);
+    upperRight.push(int);
+    upperRight.push(int2);  
+}
 
+function addRobot (answer){
+    answer = answer.split(" ", 4);
+    //The user says one letter as an orientation, so we convert it to a number
+    let cardinalNum = cardinalPoints.indexOf(answer[2]);
+    //To store the robots information we use instances of Robot Object
+    var newRobot = new Robot(answer[0], answer[1], cardinalNum, answer[3], false);
+    console.log(newRobot);
+    robots.push(newRobot);
+}
 
-rl.setPrompt('Enter the robot coordinates, orientation and instructions > ');
-rl.prompt();
-rl.on('line', (line) => {
-  line = line.split(" ", 4);
-  let cardinalNum = cardinalPoints.indexOf(line[2]);
-  var newRobot = new Robot(line[0], line[1], cardinalNum, line[3], false);
-  console.log(newRobot);
-  robots.push(newRobot);
-
-  if (a === 0){ //if there are no more robots
-    for (let i = 0; i < robots.length; i++){
-      let finalPosition = [robots[i].x, robots[i].y, robots[i].orientation]
-      finalPosition.join(' ');
-      console.log(finalPosition);
+const main = async () => {
+    bounds = await input("Enter the upper-right coordinates: ");
+    gridBounds(bounds); //bounds is a string of the grid's coordinates
+    //Then we ask for each one of the three robot's information
+    for (let i = 0; i < 3; i++) {
+        robotInfo = await input("Enter the robot coordinates, orientation and instructions: ");
+        addRobot(robotInfo); //This function adds an instance of Robot to the array robots 
+    }
+    //We move the robots and print the resulting position for each
+    for (element in robots) {
+        console.log(move(robots[element].x, robots[element].y, robots[element].orientation, robots[element].lost, upperRight[0], upperRight[1], robots[element].instructions, lostRobots, scent));
     }
     rl.close();
-  } else if (a > 0){
-    rl.setPrompt('Enter the robot coordinates, orientation and instructions > ');
-    rl.prompt();
-    a--;
-  }
-  
-}).on('close', () => {
-  console.log('Have a nice day!');
-});
+};
 
-//TO DO: Execute the program and ask for grid boundaries
+main();
--- envir.js
+++ envir.js
@@ -2,7 +2,7 @@
 // let lostRobots = [] // This array will look like [[1,2],[3,4]]
 
 
-module.exports = function turn(instruction, orientation) {
+function turn(instruction, orientation) {
     //This function changes the orientation of the robot with a string and an int given
     switch (instruction) {
         case "R":
@@ -18,43 +18,48 @@ module.exports = function turn(instruction, orientation) {
       }
 }
 
-module.exports = function move(robotX, robotY, robotOrientation, boundX, boundY, instructions, lostRobots, scent) {
-    //To follow the instructions, I'll apply a for loop
-    let orientation = robotOrientation;
-    let x = robotX;
-    let y = robotY;
-    let position = [x, y, orientation];
-    for (let i = 0; i < instructions.length; i++){
-        const command = instructions[i];
-        position[2] = command;
-        let nO = turn (command, orientation); //We calculate the new orientation
-        //we check if in this position there is a scent
-        if (scent.indexOf(position) !== -1) { 
-            continue; // We ignore the instruction because there is prohibition 
-        }
-        //We check if this orientation is within the bounds of the grid
-        if (lost(nO, x,y,boundX,boundY,lostRobots)) {
-            let newScent = [x,y];
-            scent.push(newScent);
-            break;
-        }
-        //Since this instruction is not following a scent and our robot is not getting lost, we follow it
-        switch (command) {
-            case 0:
-              y++;
-              continue;
-            case 1:
-              x++;
-              continue;
-            case 2:
-              y--;
-              continue;
-            case 3:
-              x--;
-        }
-    }
-    position.join(' ');
-    return position;
+export default function move(robotX, robotY, robotOrientation, robotLost, boundX, boundY, instructions, lostRobots, scent) {
+  //To follow the instructions, I'll apply a for loop
+  let orientation = robotOrientation;
+  let x = robotX;
+  let y = robotY;
+  let position = [x, y, orientation];
+  let isLost = robotLost;
+  for (let i = 0; i < instructions.length; i++){
+      const command = instructions[i]; //This takes the new instruction
+      position[2] = command;
+      let nO = turn(command, orientation); //We calculate the new orientation
+      //we check if in this position there is a scent
+      if (scent.indexOf(position) !== -1) { 
+          continue; // We ignore the instruction because there is prohibition 
+      }
+      //We check if this orientation is within the bounds of the grid
+      if (lost(nO, x,y,boundX,boundY,lostRobots)) {
+          let newScent = [x,y];
+          scent.push(newScent);
+          isLost = true;
+          break;
+      }
+      //Since this instruction is not following a scent and our robot is not getting lost, we follow it
+      switch (command) {
+          case 0:
+            y++;
+            continue;
+          case 1:
+            x++;
+            continue;
+          case 2:
+            y--;
+            continue;
+          case 3:
+            x--;
+      }
+  };
+  if (isLost) {
+    position.push("LOST");
+  };
+  position.join(' ');
+  return position;
 }
 
 function lost(orientation, x, y, boundX, boundY, lostRobots) {
